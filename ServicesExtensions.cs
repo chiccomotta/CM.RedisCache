688b019b68f85a4ec2f6401dbad10dd92712c895
Cristiano Motta <cristiano.motta@hotmail.it>
2024-03-31 17:20:21 +0200

    Renaming method

--- ServicesExtensions.cs
+++ ServicesExtensions.cs
@@ -5,7 +5,7 @@ namespace CM.RedisCache;
 
 public static class ServicesExtensions
 {
-    public static void AddRedisService(this IServiceCollection services, string redisConnection)
+    public static void AddRedisCacheService(this IServiceCollection services, string redisConnection)
     {
         services.AddSingleton<IConnectionMultiplexer>(opt => ConnectionMultiplexer.Connect(redisConnection));
     }
